a4446ec3969370268c26f607fd3f2a07fbad6e09
NicolasAgustin03 <programacionutn03@gmail.com>
2026-05-21 14:41:29 -0300

    Correccion de codigo y organizacion de archivos en paquete

--- Principal.py
+++ Principal.py
@@ -1,84 +1,96 @@
-from matematicas import es_par, es_primo, es_multiplo
-from validaciones import validar_rango
+from paquete.matematicas import *
+from paquete.validaciones import *
 
-def iniciar_sesion():
-    usuario1 = ""
-    contraseña1 = ""
-    usuario_creado = False
-    login = False
 
-    while login == False:
+usuario1 = ""
+contraseña1 = ""
+usuario_creado = False
+login = False
 
-        cuenta = input("Ingrese 'Registrarse' o 'Iniciar sesion': ").lower()
+while login == False:
 
-        while cuenta != "registrarse" and cuenta != "iniciar sesion":
-            cuenta = input("Por favor , Ingrese 'Registrarse' o 'Iniciar sesion': ").lower()
+    cuenta = input("Ingrese 'registrarse' o 'iniciar sesion': ")
 
-        if cuenta == "registrarse":
-            usuario1 = input("Cree su nombre de usuario: ")
-            contraseña1 = input("Cree su contraseña: ")
-            usuario_creado = True
-            print("Registro exitoso")
-            print("Inicie sesion\n")
-            usuario = input ("Ingrese su nombre de usuario: ") 
-            contraseña = input ("Ingrese su contraseña: ") 
+    while cuenta != "registrarse" and cuenta != "iniciar sesion":
+        cuenta = input("Por favor , Ingrese 'registrarse' o 'iniciar sesion': ")
+
+    if cuenta == "registrarse":
+        usuario1 = input("Cree su nombre de usuario: ")
+        contraseña1 = input("Cree su contraseña: ")
+        usuario_creado = True
+        print("Registro exitoso")
+        print("Inicie sesion\n")
+        usuario = input ("Ingrese su nombre de usuario: ") 
+        contraseña = input ("Ingrese su contraseña: ") 
         
-            while usuario != usuario1 or contraseña != contraseña1: 
-                print ("Usuario o contraseña incorrectos , intente de nuevo")             
-                usuario = input ("Ingrese su nombre de usuario: ")
-                contraseña = input ("Ingrese su contraseña: ")
+        while usuario != usuario1 or contraseña != contraseña1: 
+            print ("Usuario o contraseña incorrectos , intente de nuevo")             
+            usuario = input ("Ingrese su nombre de usuario: ")
+            contraseña = input ("Ingrese su contraseña: ")
             
-            print("Inicio de sesion exitoso")
-            login = True
+        print("Inicio de sesion exitoso")
+        login = True
             
 
-        elif cuenta == "iniciar sesion":
-            if usuario_creado == False:
-                print("No existen usuarios creados , debe registrarse primero")
-            else:
+    elif cuenta == "iniciar sesion":
+        if usuario_creado == False:
+            print("No existen usuarios creados , debe registrarse primero")
+        else:
+            usuario = input("Ingrese su nombre de usuario: ")
+            contraseña = input("Ingrese su contraseña: ")
+
+            while usuario != usuario1 or contraseña != contraseña1:
+                print("Usuario o contraseña incorrectos, intente de nuevo")
                 usuario = input("Ingrese su nombre de usuario: ")
                 contraseña = input("Ingrese su contraseña: ")
 
-                while usuario != usuario1 or contraseña != contraseña1:
-                    print("Usuario o contraseña incorrectos, intente de nuevo")
-                    usuario = input("Ingrese su nombre de usuario: ")
-                    contraseña = input("Ingrese su contraseña: ")
-
-                print("Inicio de sesion exitoso")
-                login = True
-
-def menu_de_seleccion():
-    menu = "0"
-    while menu != "6" :
-
-        menu = input("Menu Principal : \n \n 1) Proyectos \n 2) Tablas \n 3) Variables \n 4) Mostrar \n 5) Estadistica \n 6) Salir \n ")
-
-        while menu != "1" and menu != "2" and menu != "3" and menu != "4" and menu != "5" and menu != "6":
-            print("Valor invalido , Ingrese un valor correcto \n")
-            menu = input("Menu de seleccion: \n \n 1) Proyectos \n 2) Tablas \n 3) Variables \n 4) Mostrar \n 5) Estadistica \n 6) Salir \n ")
+            print("Inicio de sesion exitoso")
+            login = True
 
 
-        if menu == "1":
-            print("Generación y gestión de proyectos \n")
+menu = 0
+while menu != 6 :
 
-        elif menu == "2":
-            print("Creación y modificación de tablas \n")
+    menu = int(input(
+        "Menu De Seleccion: \n \n"
+        "1) Proyectos \n" 
+        "2) Tablas \n" 
+        "3) Variables \n" 
+        "4) Mostrar \n" 
+        "5) Estadistica \n" 
+        "6) Salir \n"))
 
-        elif menu == "3":
-            print("Creación, carga y modificación de variables \n")
-
-        elif menu == "4":
-            print("Mostrar por pantalla información disponible en una tabla \n")
-
-        elif menu == "5":
-            print("Conteos o frecuencias. Máximos y mínimos. Medidas de tendencia central: promedios aritméticos y geométricos. Medidas de dispersión. Medidas de posición. \n")
-
-        elif menu == "6":
-            print("Finalizar la ejecución del programa \n")
-
+    while validar_rango(menu , 1 , 6) == False:
+        print("Valor invalido , Ingrese un valor correcto \n")
+        menu = int(input(
+            "Menu de seleccion: \n \n" 
+            "1) Proyectos \n" 
+            "2) Tablas \n" 
+            "3) Variables \n" 
+            "4) Mostrar \n" 
+            "5) Estadistica \n" 
+            "6) Salir \n"))
 
 
-iniciar_sesion()
+    if menu == 1:
+        print("Generación y gestión de proyectos \n")
 
-menu_de_seleccion()
+    elif menu == 2:
+        print("Creación y modificación de tablas \n")
+
+    elif menu == 3:
+        print("Creación, carga y modificación de variables \n")
+
+    elif menu == 4:
+        print("Mostrar por pantalla información disponible en una tabla \n")
+
+    elif menu == 5:
+        print("Conteos o frecuencias. Máximos y mínimos. Medidas de tendencia central: promedios aritméticos y geométricos. Medidas de dispersión. Medidas de posición. \n")
+
+    elif menu == 6:
+        print("Finalizar la ejecución del programa \n")
+
+
+
+
 
--- paquete/validaciones.py
+++ paquete/validaciones.py
@@ -1,7 +1,12 @@
 def validar_rango(num:int , minimo:int , maximo:int) -> bool:
+    """
+    Determina si un numero se encuentra
+    entre un valor minimo y un valor maximo.
+    
+    """
     if num >= minimo and num <= maximo:
         return True
     else:
         return False
     
-num = validar_rango(4 , 2 , 7)
+
